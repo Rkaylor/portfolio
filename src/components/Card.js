636fd402198ddc521400cdbf59522db97fea093c
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-23 23:51:32 -0400

    Card functionality is working properly and leading to my other pages as desired.

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ import CardInfo from './CardInfo'
 function Card(props){
 
     return(
-        <div>
-            <img className=""src={props.item.imgSrc} alt={props.item.imgsrc}/>
+        <div className="g-inline-block g-card" onClick={(e) => props.click(props.item)}>
+            <img className="g-card-image"src={props.item.imgSrc} alt={props.item.imgsrc}/>
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />} 
         </div>
 
