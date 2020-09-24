0f5d0f5386060206cd3dda0988842827c55c44d8
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-23 21:54:48 -0400

    Detailed Carousel array.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,9 +4,32 @@ class Carousel extends React.Component {
 
     constructor(props){
     super(props);
-    this.state = {
+    this.state = [
+        {
+            id: 0,
+            title: "GitHub",
+            subTitle: "Repository",
+            link: "https://github.com/Rkaylor",
+            selected: false
 
-    }
+        },
+        {
+            id: 1,
+            title: "Linkden",
+            subTitle: "My Personal Linkden",
+            link: "https://www.linkedin.com/in/robert-kaylor12/",
+            selected: false
+
+        },
+        {
+            id: 2,
+            title: "Projects",
+            subTitle: "My Projects",
+            link: "https://high-score-quiz-game.herokuapp.com/",
+            selected: false
+
+        },
+    ]
 }
 
     render() {
