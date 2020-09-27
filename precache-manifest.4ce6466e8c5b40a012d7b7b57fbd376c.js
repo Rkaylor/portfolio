d0795c2c775775b12638bee8684daaf81e4de769
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-26 22:02:38 -0400

    Updates

---
+++ precache-manifest.4ce6466e8c5b40a012d7b7b57fbd376c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a152ce9759d1808e3c1bf9b886c439",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9d20d2db269c77dad3b8",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0d574466e7d4988e7846",
+    "url": "/portfolio/static/css/main.359b68d2.chunk.css"
+  },
+  {
+    "revision": "9d20d2db269c77dad3b8",
+    "url": "/portfolio/static/js/2.873fdc2b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.873fdc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d574466e7d4988e7846",
+    "url": "/portfolio/static/js/main.0db58a73.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7ad79df5285f11b9950afab8e1b187fd",
+    "url": "/portfolio/static/media/MyProject.7ad79df5.png"
+  },
+  {
+    "revision": "8d7392998769f238d0dcc282c3e0c93a",
+    "url": "/portfolio/static/media/github.8d739299.jpg"
+  },
+  {
+    "revision": "7e066d28f050fe886a6b7ddc03fe52e5",
+    "url": "/portfolio/static/media/linkden.7e066d28.png"
+  }
+]);
